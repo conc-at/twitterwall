42dc7e1b5a90b5e179d3ff8e033e578821229171
Stephan Bönnemann <stephan@boennemann.me>
2014-10-28 22:37:14 +0100

    feat: init twit

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 var path = require('path')
 
 var express = require('express')
+var Twit = require('twit')
+
+var T = new Twit({
+  /*jshint camelcase: false */
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  /*jshint camelcase: true */
+})
 
 var app = express()
 
