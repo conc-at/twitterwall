247a23b990b11ad54cce0f10d6ec6f3ee8b3ce7d
Stephan Bönnemann <stephan@boennemann.me>
2014-10-29 11:56:55 +0100

    feat: replace emoji w/emojize for retina images

--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
 var angular = require('angular')
 require('angular-sanitize')
 var link = require('twitter-text')
-var emoji = require('emoji')
+var emoji = require('emojize')
 
 angular.module('twitterwall', ['ngSanitize'])
   .controller('SiteCtrl', function($scope, $sanitize) {
     $scope.tweets = []
 
     $scope.linkTweet = function(tweet) {
-      return emoji.unifiedToHTML(link.autoLink(tweet.text, tweet.entities.urls))
+      return emoji.emojize(link.autoLink(tweet.text, tweet.entities.urls))
     }
 
     $scope.socket.on('tweet', function(tweet){
