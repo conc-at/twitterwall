e611292b069a21907bd8338a0f1cb0da4d4d59f8
Stephan Bönnemann <stephan@boennemann.me>
2014-10-29 11:14:28 +0100

    feat: convert emoji to images for chrome support

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 var angular = require('angular')
 require('angular-sanitize')
 var link = require('twitter-text')
+var emoji = require('emoji')
 
 angular.module('twitterwall', ['ngSanitize'])
   .controller('SiteCtrl', function($scope, $sanitize) {
     $scope.tweets = []
 
     $scope.linkTweet = function(tweet) {
-      return link.autoLink(tweet.text, tweet.entities.urls)
+      return emoji.unifiedToHTML(link.autoLink(tweet.text, tweet.entities.urls))
     }
 
     $scope.socket.on('tweet', function(tweet){
