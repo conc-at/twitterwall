47b9b766a688d3e97c8d1a5e55b39a772bcd4f20
Stephan Bönnemann <stephan@boennemann.me>
2014-10-29 10:23:25 +0100

    feat: render tweets as html

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 var angular = require('angular')
+require('angular-sanitize')
+var link = require('twitter-text')
 
-angular.module('twitterwall', [])
-  .controller('SiteCtrl', function($scope) {
+angular.module('twitterwall', ['ngSanitize'])
+  .controller('SiteCtrl', function($scope, $sanitize) {
     $scope.tweets = []
+
+    $scope.linkTweet = function(tweet) {
+      return link.autoLink(tweet.text, tweet.entities.urls)
+    }
 
     $scope.socket.on('tweet', function(tweet){
       $scope.tweets.unshift(tweet)
