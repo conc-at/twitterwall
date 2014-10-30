2e99505b1648dd40875a4e6f2e0aaee7c669f492
Christoph Witzko <github@christophwitzko.com>
2014-10-30 14:20:15 +0100

    feat: make debug global

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var http = require('http')
 
-var debug = require('debug')('twitterwall')
 var express = require('express')
 var Twit = require('twit')
 
@@ -11,6 +10,7 @@ var lib = require('./lib')
 var app = express()
 var server = http.Server(app)
 
+app.debug = require('debug')('twitterwall')
 app.configjs = require('./config')
 app.socketio = require('socket.io')(server)
 app.twitter = new Twit(app.configjs.twitter.auth)
@@ -19,28 +19,28 @@ app.configjs.admin.blocked = app.configjs.admin.blocked || []
 lib.middlewares(app)
 lib.routes(app)
 
-debug('resolving screen names')
+app.debug('resolving screen names')
 app.twitter.get('users/lookup', {screen_name: app.configjs.twitter.users.join(',')}, function (err, data, response) {
-  if(err) return debug('error: %s', err)
+  if(err) return app.debug('error: %s', err)
   var userids = []
   data.forEach(function(u){
     userids.push(u.id_str)
   })
   var searchConf = lib.twitter.config.parse(app.configjs.twitter.tracks, app.configjs.twitter.users, userids)
-  debug('starting streams...')
+  app.debug('starting streams...')
   var hashStream = app.twitter.stream('statuses/filter', searchConf.hashtags)
   var userStream = app.twitter.stream('statuses/filter', searchConf.users)
   var stream = lib.twitter.unify([hashStream, userStream])
   var tweetBuffer = require('parallizer').Parallel(1)
 
   stream.on('tweet', function (tweet) {
-    debug('tweet: %s', tweet.text)
-    if(lib.twitter.block(tweet, app.configjs.admin.blocked)) return debug('tweet blocked')
+    app.debug('tweet: %s', tweet.text)
+    if(lib.twitter.block(tweet, app.configjs.admin.blocked)) return app.debug('tweet blocked')
     tweetBuffer.sadd(lib.utils.throttleDelay(app.configjs.twitter.throttle), tweet, app.socketio.emit.bind(io, 'tweet'))
   })
 
   stream.on('error', function (err) {
-    debug('error: %s', err.message)
+    app.debug('error: %s', err.message)
   })
 
   app.socketio.on('connection', function(socket){
@@ -48,7 +48,7 @@ app.twitter.get('users/lookup', {screen_name: app.configjs.twitter.users.join(',
       q: searchConf.qstr,
       count: 10
     }, function(err, data, response) {
-      if(err) return
+      if(err) return app.debug('error: %s', err)
       lib.twitter.stagger(socket, data.statuses.reverse())
     })
   })
@@ -62,5 +62,5 @@ app.socketio.of('/test').on('connection', function(socket){
 var port = process.env.PORT || 8000
 
 server.listen(port, function() {
-  debug('Listening on %d', port)
+  app.debug('Listening on %d', port)
 })
