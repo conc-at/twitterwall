879cd284edca28edb4a3043dc84f3974fc9aaf3d
Stephan Bönnemann <stephan@boennemann.me>
2014-10-30 22:21:24 +0100

    feat: render basic schedule

--- app/index.js
+++ app/index.js
@@ -12,10 +12,29 @@ function avatar(url) {
 }
 
 var app = angular.module('twitterwall', ['ngSanitize', 'ngAnimate'])
-  .controller('SiteCtrl', function($scope, $sanitize) {
+  .controller('SiteCtrl', function($scope, $sanitize, $http) {
     $scope.config = require('../config')
 
     $scope.tweets = []
+
+    $scope.lanyrd = []
+
+    $http.get('/schedule').then(function(res) {
+      var dataSet
+      var today = (new Date()).toISOString().slice(0,10)
+
+      Object.keys(res.data).forEach(function(date) {
+        if (date === today) {
+          dataSet = res.data[date]
+        }
+      })
+
+      if (!dataSet) {
+        dataSet = res.data[Object.keys(res.data)[0]]
+      }
+
+      $scope.lanyrd = dataSet
+    })
 
     $scope.linkTweet = function(tweet) {
       return emoji.emojize(link.autoLink(tweet.text, tweet.entities.urls))
