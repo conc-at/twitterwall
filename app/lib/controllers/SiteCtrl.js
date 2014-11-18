e7980cea546c4d62764396734ca0fe55d002b093
Christoph Witzko <github@christophwitzko.com>
2014-11-18 19:08:49 +0100

    feat: set flash duration

--- app/lib/controllers/SiteCtrl.js
+++ app/lib/controllers/SiteCtrl.js
@@ -1,18 +1,22 @@
 'use strict'
 
+var twemoji = require('twemoji')
+
 module.exports = function(app) {
   app.controller('SiteCtrl', function($scope, $timeout, socket, config) {
     $scope.config = config
 
     var $flash = angular.element(document.querySelector('#flash'))
 
-    socket.on('flash', function(message){
-      $scope.message = message
+    socket.on('flash', function(flash){
+      if(!flash.message) return $flash.removeClass('active')
+      $scope.message = twemoji.parse(flash.message, {size: 72})
       $scope.$apply()
       $flash.addClass('active')
+      if(flash.duration === 0) return
       $timeout(function() {
         $flash.removeClass('active')
-      }, 1e4)
+      }, flash.duration)
     })
   })
 }
