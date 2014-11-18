97ad4c536b1b1126024efef8338c97a93c9c87b0
Christoph Witzko <github@christophwitzko.com>
2014-11-18 15:55:15 +0100

    feat: flash socket.io event

--- app/lib/controllers/SiteCtrl.js
+++ app/lib/controllers/SiteCtrl.js
@@ -1,7 +1,11 @@
 'use strict'
 
 module.exports = function(app) {
-  app.controller('SiteCtrl', function($scope, config) {
+  app.controller('SiteCtrl', function($scope, socket, config) {
     $scope.config = config
+
+    socket.on('flash', function(message){
+      console.log(message)
+    })
   })
 }
