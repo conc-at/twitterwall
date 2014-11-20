f0f89ea46383282330e0e0ead4850f0c9a6ccb4f
Christoph Witzko <github@christophwitzko.com>
2014-11-20 15:01:11 +0100

    feat: added highlight event

--- app/lib/controllers/SiteCtrl.js
+++ app/lib/controllers/SiteCtrl.js
@@ -3,5 +3,9 @@
 module.exports = function(app) {
   app.controller('SiteCtrl', function($scope, $timeout, socket, config) {
     $scope.config = config
+
+    socket.on('highlight', function(tweet){
+      console.log(tweet.text)
+    })
   })
 }
