ba6d61b67bf82a448ec5c6633e9892013a1728a1
Christoph Witzko <github@christophwitzko.com>
2014-12-01 19:09:49 +0100

    feat: add flash message countdown

--- app/lib/directives/flash-message.js
+++ app/lib/directives/flash-message.js
@@ -4,11 +4,13 @@ var markdown = require('markdown').markdown
 var twemoji = require('twemoji')
 
 module.exports = function(app) {
-  app.directive('flashMessage', function($animate, $timeout, socket) {
+  app.directive('flashMessage', function($animate, $interval, socket) {
     return {
       scope: {},
       template: '<span ng-bind-html="message"></span>',
       link: function(scope, element) {
+        var doHide = false
+        var doneTs = 0
 
         function show() {
           $animate.addClass(element, 'flash-message')
@@ -18,20 +20,28 @@ module.exports = function(app) {
           $animate.removeClass(element, 'flash-message')
         }
 
-        function cancel() {
-          if (timeout) $timeout.cancel(timeout)
+        function tick(){
+          if(!doHide) return
+          var cTs = (new Date()).getTime()
+          if(cTs >= doneTs){
+            hide()
+            doHide = false
+          }
+          else{
+            var sl = (doneTs - cTs)/1000
+            console.log(sl.toFixed(0))
+          }
         }
 
         function svgify(icon) {
           return 'https://twemoji.maxcdn.com/svg/' + icon + '.svg'
         }
 
-        var timeout = null;
+        $interval(tick, 100)
+
         socket.on('flash', function(flash){
-          if (!flash.message) {
-            hide()
-            return cancel()
-          }
+          doHide = false
+          if (!flash.message) return hide()
           if (flash.markdown) {
             console.log('detected markdown')
             flash.message = markdown.toHTML(flash.message)
@@ -39,9 +49,9 @@ module.exports = function(app) {
           scope.message = twemoji.parse(flash.message, svgify)
           scope.$apply()
           show()
-          cancel()
-          if(flash.duration === 0) return
-          timeout = $timeout(hide, flash.duration)
+          if(!flash.duration) return
+          doneTs = (new Date()).getTime() + flash.duration
+          doHide = true
         })
 
       }
