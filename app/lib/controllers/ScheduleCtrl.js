4d38534124b5f5a46dd14f27213739d888f34f8d
Christoph Witzko <github@christophwitzko.com>
2014-12-05 15:25:16 +0100

    feat: added multiple track support

--- app/lib/controllers/ScheduleCtrl.js
+++ app/lib/controllers/ScheduleCtrl.js
@@ -6,40 +6,53 @@ module.exports = function(app) {
     var forerun = 10 // in minutes
     var todayData
 
-    function getCurrentTalks(){
-      var currentTalks = {}
+    function getNextTalks(){
+      var nextTalks = {}
       var now = new Date()
       var compare = new Date(today)
-      compare.setMinutes(now.getMinutes())
       compare.setHours(now.getHours())
+      compare.setMinutes(now.getMinutes())
       Object.keys(todayData).forEach(function(room) {
         todayData[room].some(function(v){
           var time = new Date(v.start_time)
           if (((time - compare) / 1000 * 60 * 60) > forerun) {
-            currentTalks[room] = v
+            nextTalks[room] = v
             return true
           }
         })
       })
-      return currentTalks
+      return nextTalks
     }
+
+    function talksToArray(nextTalks){
+      var ret = []
+      Object.keys(nextTalks).forEach(function(v){
+        ret.push(nextTalks[v])
+      })
+      return ret
+    }
+
     $http.get('/schedule').then(function(res) {
       var dataSet = {}
-
       Object.keys(res.data).forEach(function(room) {
         Object.keys(res.data[room]).forEach(function(date){
           if(date === today) dataSet[room] = res.data[room][date]
         })
       })
-
       todayData = dataSet
-      $scope.nextUp = getCurrentTalks()
-      console.log($scope.nextUp)
     })
 
-
+    var currentIndex = 0
     $interval(function() {
-      // TODO
+      var nextTalks = talksToArray(getNextTalks())
+      if(nextTalks[currentIndex]){
+        $scope.nextUp = nextTalks[currentIndex]
+        currentIndex++
+      }
+      else{
+        if(nextTalks[0]) $scope.nextUp = nextTalks[0]
+        currentIndex = 1
+      }
     }, 5000)
   })
 }
