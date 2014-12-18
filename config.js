67976e2681dc9fd37a2d7d0d29c76a5fdd96df3a
Christoph Witzko <github@christophwitzko.com>
2014-12-18 09:03:01 +0100

    feat: added room colors to config

--- config.js
+++ config.js
@@ -22,7 +22,12 @@ module.exports = {
   lanyrd: {
     year: e.LANYRD_YEAR || '2014',
     id: e.LANYRD_ID || 'barcamp-salzburg-october',
-    showNext: 5*60*1000
+    showNext: 5*60*1000,
+    roomColors: {
+      '_': '#000000',
+      '017': '#FF3434',
+      '018': '#34FF34'
+    }
   },
   admin: {
     enableAPI: e.ADMIN_USER && e.ADMIN_PASSWORD,
