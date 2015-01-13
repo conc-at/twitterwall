a6aac1b7276625c79fd54c34fb30a236971cbdbd
Christoph Witzko <github@christophwitzko.com>
2015-01-13 15:52:03 +0100

    refactor: removed highlight event

--- app/lib/controllers/SiteCtrl.js
+++ app/lib/controllers/SiteCtrl.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.controller('SiteCtrl', function($scope, $timeout, socket, config) {
     $scope.config = config
 
-    socket.on('highlight', function(tweet){
+    /*socket.on('highlight', function(tweet){
       console.log(tweet.text)
-    })
+    })*/
   })
 }
