451e98612ab1b6c56dd4248d4fbd6fb2aca9b764
Stephan Bönnemann <stephan@boennemann.me>
2015-02-18 17:44:52 +0100

    feat: proper timer for flash messages

--- app/lib/directives/flash-message.js
+++ app/lib/directives/flash-message.js
@@ -7,76 +7,48 @@ var markdown = require('markdown').markdown
 var twemoji = require('twemoji')
 
 module.exports = function (app) {
-  app.directive('flashMessage', function ($animate, $interval, socket) {
+  app.directive('flashMessage', function ($animate, $timeout, socket) {
     return {
       scope: {},
-      template: '<span ng-bind-html="message"></span><span class="timer">10</span>',
+      templateUrl: 'flash.html',
       link: function (scope, element) {
-        var doHide = false
-        var doneTs = 0
-        var timeLeft = 0
-        var timerEl = angular.element(document.querySelector('.timer'))
 
         function show () {
-          $animate.addClass(element, 'flash-message')
+          return $animate.addClass(element, 'flash-message')
         }
 
-        function hide () {
-          $animate.removeClass(element, 'flash-message')
-        }
+        function clear () {
+          return $animate.removeClass(element, 'flash-message')
 
-        function showTimer () {
-          timerEl.addClass('show')
-        }
-
-        function hideTimer () {
-          timerEl.removeClass('show')
-        }
-
-        function updateTimer () {
-          timerEl.text(timeLeft)
-        }
-
-        function tick () {
-          if (!doHide) return
-          var cTs = (new Date()).getTime()
-          if (cTs >= doneTs) {
-            hide()
-            doHide = false
-          } else {
-            var tl = Math.round((doneTs - cTs) / 1000)
-            if (timeLeft !== tl) {
-              timeLeft = tl
-              updateTimer()
-            }
-          }
+          .then(function () {
+            scope.flash = null
+          })
         }
 
         function svgify (icon) {
           return 'https://twemoji.maxcdn.com/svg/' + icon + '.svg'
         }
 
-        $interval(tick, 100)
-
+        var timeout
         socket.on('flash', function (flash) {
-          doHide = false
-          timeLeft = 0
-          hideTimer()
-          if (!flash.message) return hide()
-          if (flash.markdown) {
-            console.log('detected markdown')
-            flash.message = markdown.toHTML(flash.message)
-          }
-          scope.message = twemoji.parse(flash.message, svgify)
-          if (flash.duration) {
-            timeLeft = Math.round(flash.duration / 1000)
-            doneTs = (new Date()).getTime() + flash.duration
-            doHide = true
-            showTimer()
-            updateTimer()
-          }
-          scope.$apply()
-          show()
+          clear()
+
+          .then(function () {
+            if (timeout) $timeout.cancel(timeout)
+            if (!flash.message) return
+            if (flash.markdown) flash.message = markdown.toHTML(flash.message)
+            flash.message = twemoji.parse(flash.message, svgify)
+
+            scope.flash = flash
+
+            show()
+
+            .then(function () {
+              if (!flash.duration) return
+
+              timeout = $timeout(clear, flash.duration)
+            })
+          })
         })
       }
     }
